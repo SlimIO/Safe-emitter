52c365020296b497790aeb71b65bdb2685a1c82e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-28 15:34:19 +0100

    chore: improve d.ts definition

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare class SafeEmitter<T> {
+declare class SafeEmitter<T = SafeEmitter.Default> {
     constructor();
 
     // Static Properties
@@ -10,14 +10,14 @@ declare class SafeEmitter<T> {
     catch(errorListener: SafeEmitter.ErrorListener<T>): this;
     eventNames(): keyof T[];
     listenerCount<K extends keyof T>(eventName: K): number;
-    listeners<K extends keyof T>(eventName: K): SafeEmitter.Listener<T[K]>[];
-    rawListeners<K extends keyof T>(eventName: K): SafeEmitter.Listener<T[K]>[];
-    on<K extends keyof T>(eventName: K, listener: SafeEmitter.Listener<T[K]>): void;
-    off<K extends keyof T>(eventName: K, listener: SafeEmitter.Listener<T[K]>): boolean;
+    listeners<K extends keyof T>(eventName: K): T[K][];
+    rawListeners<K extends keyof T>(eventName: K): T[K][];
+    on<K extends keyof T>(eventName: K, listener: T[K]): void;
+    off<K extends keyof T>(eventName: K, listener: T[K]): boolean;
     once<K extends keyof T>(eventName: K, timeOut?: number): Promise<[T[K]]>;
-    addEventListener<K extends keyof T>(eventName: K, listener: SafeEmitter.Listener<T[K]>): void;
-    removeEventListener<K extends keyof T>(eventName: K, listener: SafeEmitter.Listener<T[K]>): void;
-    prependListener<K extends keyof T>(eventName: K, listener: SafeEmitter.Listener<T[K]>): void;
+    addEventListener<K extends keyof T>(eventName: K, listener: T[K]): void;
+    removeEventListener<K extends keyof T>(eventName: K, listener: T[K]): void;
+    prependListener<K extends keyof T>(eventName: K, listener: T[K]): void;
     prependOnceListener(): void;
     removeAllListeners<K extends keyof T>(eventName?: K): void;
     emit<K extends keyof T>(eventName: K, data: T[K]): void;
@@ -26,9 +26,10 @@ declare class SafeEmitter<T> {
 
 declare namespace SafeEmitter {
     export type EventType = string | symbol;
-
-    export type ErrorListener<T> = (error: Error, eventName: keyof T, listener: Listener<T[keyof T]>) => void;
-    export type Listener<R> = (argument: R) => void;
+    export type ErrorListener<T> = (error: Error, eventName: keyof T, listener: T[keyof T]) => void;
+    export interface Default {
+        [key: string]: () => void;
+    }
 }
 
 export as namespace SafeEmitter;
